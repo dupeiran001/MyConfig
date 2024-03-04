21d221468dad8d953f41022e7e6f5f69a30eed12
杜沛然 <2083856903@qq.com>
2024-03-04 17:54:42 +0800

    add lazygit noice telescope

---
+++ nvim/lua/plugins/lazygit.lua
@@ -0,0 +1,17 @@
+return  {
+  "kdheepak/lazygit.nvim",
+  cmd = {
+    "LazyGit",
+    "LazyGitConfig",
+    "LazyGitCurrentFile",
+    "LazyGitFilter",
+    "LazyGitFilterCurrentFile",
+  },
+  keys = {
+    { "<leader>gg", "<cmd>LazyGit<cr>", desc = "LazyGit" },
+  },
+  -- optional for floating window border decoration
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+  },
+}
