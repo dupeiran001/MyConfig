08fbfff690590edb1b73ab2c37990898684a1baf
杜沛然 <2083856903@qq.com>
2024-03-07 14:05:55 +0800

    add navic

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -9,13 +9,31 @@ return {
         section_separators = { left = '', right = '' },
       },
       sections = {
-        lualine_a = {{ "mode", separator = { left = '' }, right_padding = 2 }},
-        lualine_z = {{
+        lualine_a = { { "mode", separator = { left = '' }, right_padding = 2 } },
+        lualine_c = {
+          {
+            "diagnostics",
+          },
+          {
+            "filetype",
+            icon_only = true,
+            separator = "",
+            padding = { left = 1, right = 0 }
+          },
+          {
+            'filename',
+          },
+
+          { "%{%v:lua.require'nvim-navic'.get_location()%}", separator = { left = ">" } },
+        },
+        lualine_z = { {
           function()
             return " " .. os.date("%R")
           end,
-          separator = { right = '' }, left_padding = 2
-        }}
+          separator = { right = '' },
+          left_padding = 2
+        } }
       },
-  }
-end}
+    }
+  end
+}
