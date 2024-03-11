383d6769518a002a3b745f184ccf56472d1a623d
杜沛然 <2083856903@qq.com>
2024-03-11 13:34:08 +0800

    edit nvim_cmp keymap

--- nvim/lua/plugins/nvim_cmp.lua
+++ nvim/lua/plugins/nvim_cmp.lua
@@ -1,11 +1,16 @@
 return {
   "hrsh7th/nvim-cmp",
   version = false, -- last release is way too old
+  keys = {
+    { "<Tab>", false }
+  },
   opts = function(_, opts)
     local cmp = require("cmp")
     opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Insert }),
-      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Insert }),
+      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+      -- FIXME: unable to override this Tab keymap
+      ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
     })
   end,
- }
+}
