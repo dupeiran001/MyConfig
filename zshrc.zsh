9a1747402be639bec5cc79bae3c65b9b775f391c
杜沛然 <2083856903@qq.com>
2024-03-12 15:51:12 +0800

    change to starship prompt

--- zshrc.zsh
+++ zshrc.zsh
@@ -93,26 +93,7 @@ source $ZSH/oh-my-zsh.sh
 
 KEYTIMEOUT=1
 
-#fpath+=($HOME/.oh-my-zsh/themes/pure)
-#autoload -U promptinit; promptinit
-#prompt pure
-#
-#
-#export PURE_PROMPT_SYMBOL="❯"
-#export PURE_PROMPT_VICMD_SYMBOL="%{$fg[cyan]%}❮%{$reset_color%}"
-
-# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
-# Initialization code that may require console input (password prompts, [y/n]
-# confirmations, etc.) must go above this block; everything else may go below.
-if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
-  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
-fi
-
-source ~/.config/resources/powerlevel10k/powerlevel10k.zsh-theme
-
-
-# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
-[[ ! -f ~/.config/p10k.zsh ]] || source ~/.config/p10k.zsh
+eval "$(starship init zsh)"
 
 bindkey -v
 
