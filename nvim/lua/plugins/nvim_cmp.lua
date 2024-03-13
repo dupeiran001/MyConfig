a1cdaf62b492c9ca7da47ff34669471a209f5bf9
杜沛然 <2083856903@qq.com>
2024-03-13 18:14:36 +0800

    fix nvim cmp

--- nvim/lua/plugins/nvim_cmp.lua
+++ nvim/lua/plugins/nvim_cmp.lua
@@ -1,16 +1,22 @@
 return {
-  "hrsh7th/nvim-cmp",
-  version = false, -- last release is way too old
-  keys = {
-    { "<Tab>", false }
+  {
+    "hrsh7th/nvim-cmp",
+    version = false, -- last release is way too old
+    -- keys = {
+    --   { "<Tab>", false },
+    -- },
+    opts = function(_, opts)
+      local cmp = require("cmp")
+      opts.mapping = vim.tbl_extend("force", opts.mapping, {
+        ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+        ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
+      })
+    end,
   },
-  opts = function(_, opts)
-    local cmp = require("cmp")
-    opts.mapping = vim.tbl_extend("force", opts.mapping, {
-      ["<C-j>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-      ["<C-k>"] = cmp.mapping.select_prev_item({ behavior = cmp.SelectBehavior.Select }),
-      -- FIXME: unable to override this Tab keymap
-      ["<Tab>"] = cmp.mapping.select_next_item({ behavior = cmp.SelectBehavior.Select }),
-    })
-  end,
+  {
+    "L3MON4D3/LuaSnip",
+    keys = function() end,
+  },
 }
