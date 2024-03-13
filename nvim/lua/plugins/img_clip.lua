87bc189cfe11f5f4c762285bb1090f0ff37c76e2
杜沛然 <2083856903@qq.com>
2024-03-13 17:54:06 +0800

    add image paste and drop

---
+++ nvim/lua/plugins/img_clip.lua
@@ -0,0 +1,9 @@
+return {
+  "HakonHarnes/img-clip.nvim",
+  event = "BufEnter",
+  opts = {},
+  keys = {
+    -- suggested keymap
+    { "<leader>P", "<cmd>PasteImage<cr>", desc = "Paste clipboard image" },
+  },
+}
