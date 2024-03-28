9ceaa2507dfc9d21072a138a4b01e2386a7066f0
杜沛然 <2083856903@qq.com>
2024-03-28 10:44:41 +0800

    change lualine appearance

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -3,13 +3,15 @@ return {
 
   opts = function()
     -- bubble theme
+    local icons = require("lazyvim.config").icons
     return {
       options = {
-        component_separators = '|',
-        section_separators = { left = '', right = '' },
+        component_separators = "|",
+        section_separators = { left = "", right = "" },
       },
       sections = {
-        lualine_a = { { "mode", separator = { left = '' }, right_padding = 2 } },
+        lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
+        lualine_b = { "branch" },
         lualine_c = {
           {
             "filetype",
@@ -18,22 +20,71 @@ return {
             padding = { left = 1, right = 0 },
           },
           {
-            'filename',
+            "filename",
           },
-
           {
             "%{%v:lua.require'nvim-navic'.get_location()%}",
-            separator = { left = "" }
+            padding = { left = 1, right = 0 },
+            separator = { left = "" },
           },
         },
-        lualine_z = { {
-          function()
-            return " " .. os.date("%R")
-          end,
-          separator = { right = '' },
-          left_padding = 2
-        } }
+        lualine_x = {
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.command.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.command.has() end,
+            color = LazyVim.ui.fg("Statement"),
+          },
+          -- stylua: ignore
+          {
+            function() return require("noice").api.status.mode.get() end,
+            cond = function() return package.loaded["noice"] and require("noice").api.status.mode.has() end,
+            color = LazyVim.ui.fg("Constant"),
+          },
+          -- stylua: ignore
+          {
+            function() return "  " .. require("dap").status() end,
+            cond = function() return package.loaded["dap"] and require("dap").status() ~= "" end,
+            color = LazyVim.ui.fg("Debug"),
+          },
+          {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = LazyVim.ui.fg("Special"),
+          },
+          {
+            "diff",
+            symbols = {
+              added = icons.git.added,
+              modified = icons.git.modified,
+              removed = icons.git.removed,
+            },
+            source = function()
+              local gitsigns = vim.b.gitsigns_status_dict
+              if gitsigns then
+                return {
+                  added = gitsigns.added,
+                  modified = gitsigns.changed,
+                  removed = gitsigns.removed,
+                }
+              end
+            end,
+          },
+        },
+        lualine_y = {
+          { "progress", separator = " ", padding = { left = 1, right = 0 } },
+          { "location", padding = { left = 0, right = 1 } },
+        },
+        lualine_z = {
+          {
+            function()
+              return " " .. os.date("%R")
+            end,
+            separator = { right = "" },
+            left_padding = 2,
+          },
+        },
       },
     }
-  end
+  end,
 }
