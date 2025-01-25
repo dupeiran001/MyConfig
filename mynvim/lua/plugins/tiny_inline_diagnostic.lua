f9cf7d09002a6bfab09a66a92afba4f45691943b
Peiran Du <dupeiran3@gmail.com>
2025-01-24 22:42:48 -0600

    feat

--- mynvim/lua/plugins/tiny_inline_diagnostic.lua
+++ mynvim/lua/plugins/tiny_inline_diagnostic.lua
@@ -1,105 +1,102 @@
 return {
-  "rachartier/tiny-inline-diagnostic.nvim",
-  --event = "VeryLazy", -- Or `LspAttach`
-  opts = {
-    preset = "modern",   -- Can be: "modern", "classic", "minimal", "powerline", ghost", "simple", "nonerdfont", "amongus"
-    hi = {
-      error = "DiagnosticError",
-      warn = "DiagnosticWarn",
-      info = "DiagnosticInfo",
-      hint = "DiagnosticHint",
-      arrow = "NonText",
-      background = "CursorLine", -- Can be a highlight or a hexadecimal color (#RRGGBB)
-      mixing_color = "#3F3F3F", -- Can be None or a hexadecimal color (#RRGGBB). Used to blend the background color with the diagnostic background color with another color.
-    },
-    options = {
-      -- Show the source of the diagnostic.
-      show_source = false,
+	"rachartier/tiny-inline-diagnostic.nvim",
+	--event = "VeryLazy", -- Or `LspAttach`
+	opts = {
+		preset = "modern", -- Can be: "modern", "classic", "minimal", "powerline", ghost", "simple", "nonerdfont", "amongus"
+		hi = {
+			arrow = "NonText",
+			background = "CursorLine", -- Can be a highlight or a hexadecimal color (#RRGGBB)
+			mixing_color = "#3F3F3F", -- Can be None or a hexadecimal color (#RRGGBB). Used to blend the background color with the diagnostic background color with another color.
+		},
 
-      -- Use your defined signs in the diagnostic config table.
-      use_icons_from_diagnostic = false,
+		options = {
+			-- Show the source of the diagnostic.
+			show_source = false,
 
-      -- Add messages to the diagnostic when multilines is enabled
-      add_messages = true,
+			-- Use your defined signs in the diagnostic config table.
+			use_icons_from_diagnostic = false,
 
-      -- Throttle the update of the diagnostic when moving cursor, in milliseconds.
-      -- You can increase it if you have performance issues.
-      -- Or set it to 0 to have better visuals.
-      throttle = 0,
+			-- Add messages to the diagnostic when multilines is enabled
+			add_messages = true,
 
-      -- The minimum length of the message, otherwise it will be on a new line.
-      softwrap = 50,
+			-- Throttle the update of the diagnostic when moving cursor, in milliseconds.
+			-- You can increase it if you have performance issues.
+			-- Or set it to 0 to have better visuals.
+			throttle = 0,
 
-      -- If multiple diagnostics are under the cursor, display all of them.
-      multiple_diag_under_cursor = true,
+			-- The minimum length of the message, otherwise it will be on a new line.
+			softwrap = 50,
 
-      -- Enable diagnostic message on all lines.
-      -- Can either be a boolean or a table with the following options:
-      -- multilines = {
-      -- 	-- Enable the multilines feature
-      --- 	enabled = true,
-      --
-      -- 	-- Always show diagnostic messages on all lines
-      --  	always_show = true,
-      -- },
-      --
-      -- If it is a boolean set as true, it will enable the feature with the default options:
-      --  multilines = {
-      -- 	enabled = true,
-      -- 	always_show = false,
-      -- }
+			-- If multiple diagnostics are under the cursor, display all of them.
+			multiple_diag_under_cursor = true,
 
-      multilines = {
-        enabled = true,
-        always_show = true,
-      },
+			-- Enable diagnostic message on all lines.
+			-- Can either be a boolean or a table with the following options:
+			-- multilines = {
+			-- 	-- Enable the multilines feature
+			--- 	enabled = true,
+			--
+			-- 	-- Always show diagnostic messages on all lines
+			--  	always_show = true,
+			-- },
+			--
+			-- If it is a boolean set as true, it will enable the feature with the default options:
+			--  multilines = {
+			-- 	enabled = true,
+			-- 	always_show = false,
+			-- }
 
-      -- Show all diagnostics on the cursor line
-      show_all_diags_on_cursorline = true,
+			multilines = {
+				enabled = true,
+				always_show = true,
+			},
 
-      -- Enable diagnostics on Insert mode. You should also se the `throttle` option to 0, as some artefacts may appear.
-      enable_on_insert = true,
+			-- Show all diagnostics on the cursor line
+			show_all_diags_on_cursorline = true,
 
-      overflow = {
-        -- Manage the overflow of the message.
-        --    - wrap: when the message is too long, it is then displayed on multiple lines.
-        --    - none: the message will not be truncated.
-        --    - oneline: message will be displayed entirely on one line.
-        mode = "wrap",
-      },
+			-- Enable diagnostics on Insert mode. You should also se the `throttle` option to 0, as some artefacts may appear.
+			enable_on_insert = true,
 
-      -- Format the diagnostic message.
-      -- Example:
-      -- format = function(diagnostic)
-      --     return diagnostic.message .. " [" .. diagnostic.source .. "]"
-      -- end,
-      format = nil,
+			overflow = {
+				-- Manage the overflow of the message.
+				--    - wrap: when the message is too long, it is then displayed on multiple lines.
+				--    - none: the message will not be truncated.
+				--    - oneline: message will be displayed entirely on one line.
+				mode = "wrap",
+			},
 
-      --- Enable it if you want to always have message with `after` characters length.
-      break_line = {
-        enabled = true,
-        after = 30,
-      },
+			-- Format the diagnostic message.
+			-- Example:
+			-- format = function(diagnostic)
+			--     return diagnostic.message .. " [" .. diagnostic.source .. "]"
+			-- end,
+			format = nil,
 
-      virt_texts = {
-        priority = 2048,
-      },
+			--- Enable it if you want to always have message with `after` characters length.
+			break_line = {
+				enabled = true,
+				after = 30,
+			},
 
-      -- Filter by severity.
-      severity = {
-        vim.diagnostic.severity.ERROR,
-        vim.diagnostic.severity.WARN,
-        vim.diagnostic.severity.INFO,
-        vim.diagnostic.severity.HINT,
-      },
+			virt_texts = {
+				priority = 2048,
+			},
 
-      -- Overwrite events to attach to a buffer. You should not change it, but if the plugin
-      -- does not works in your configuration, you may try to tweak it.
-      overwrite_events = nil,
-    },
-    priority = 1000, -- needs to be loaded in first
-  },
-  init = function()
-    vim.diagnostic.config({ virtual_text = false })
-  end
+			-- Filter by severity.
+			severity = {
+				vim.diagnostic.severity.ERROR,
+				vim.diagnostic.severity.WARN,
+				vim.diagnostic.severity.INFO,
+				vim.diagnostic.severity.HINT,
+			},
+
+			-- Overwrite events to attach to a buffer. You should not change it, but if the plugin
+			-- does not works in your configuration, you may try to tweak it.
+			overwrite_events = nil,
+		},
+		priority = 1000, -- needs to be loaded in first
+	},
+	init = function()
+		vim.diagnostic.config({ virtual_text = false })
+	end,
 }
