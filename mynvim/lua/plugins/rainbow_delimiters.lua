fde858e2afbfa5613fed5bf09a991db6f314b07d
Peiran Du <dupeiran3@gmail.com>
2025-01-24 23:37:10 -0600

    feat: remove some highlight

--- mynvim/lua/plugins/rainbow_delimiters.lua
+++ mynvim/lua/plugins/rainbow_delimiters.lua
@@ -1,33 +1,33 @@
 return {
-  'HiPhish/rainbow-delimiters.nvim',
-  dependencies = { "nvim-treesitter/nvim-treesitter" },
-  opts = function()
-    local rainbow_delimiters = require 'rainbow-delimiters'
-    return {
-      strategy = {
-        [''] = rainbow_delimiters.strategy['global'],
-        vim = rainbow_delimiters.strategy['local'],
-      },
-      query = {
-        [''] = 'rainbow-delimiters',
-        lua = 'rainbow-blocks',
-      },
-      priority = {
-        [''] = 110,
-        lua = 210,
-      },
-      highlight = {
-        'RainbowDelimiterOrange',
-        'RainbowDelimiterYellow',
-        'RainbowDelimiterGreen',
-        'RainbowDelimiterCyan',
-        'RainbowDelimiterBlue',
-        'RainbowDelimiterViolet',
-        'RainbowDelimiterRed',
-      }
-    }
-  end,
-  config = function(_, opts)
-    require("rainbow-delimiters.setup").setup(opts)
-  end,
+	"HiPhish/rainbow-delimiters.nvim",
+	dependencies = { "nvim-treesitter/nvim-treesitter" },
+	opts = function()
+		local rainbow_delimiters = require("rainbow-delimiters")
+		return {
+			strategy = {
+				[""] = rainbow_delimiters.strategy["global"],
+				vim = rainbow_delimiters.strategy["local"],
+			},
+			query = {
+				[""] = "rainbow-delimiters",
+				lua = "rainbow-blocks",
+			},
+			priority = {
+				[""] = 110,
+				lua = 210,
+			},
+			highlight = {
+				"Normal",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterCyan",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterViolet",
+			},
+		}
+	end,
+	config = function(_, opts)
+		require("rainbow-delimiters.setup").setup(opts)
+	end,
 }
