d2987e748fc56429968f04c54fd9c8df1a4542f7
Peiran Du <dupeiran3@gmail.com>
2025-02-06 12:41:03 -0600

    feat: add rofi, awesome wm support

--- wezterm/fonts.lua
+++ wezterm/fonts.lua
@@ -4,21 +4,22 @@ local wezterm = require("wezterm")
 
 -- wezTerm has a nerd font fallback, so use a non-patched version here.
 if wezterm.target_triple == 'x86_64-unknown-linux-gnu' then
-    M.font = wezterm.font("Cascadia Code")
-    M.font_size = 12
-    M.bold_brightens_ansi_colors = true
-    M.freetype_load_target = "Normal"
-    M.line_height = 1
-    M.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
-    M.underline_position = -7
+  M.font = wezterm.font_with_fallback { "Cascadia Code", "KanjiStrokeOrders" }
+  M.font_size = 10
+  M.bold_brightens_ansi_colors = true
+  M.freetype_load_target = "Normal"
+  M.line_height = 1
+  M.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
+  M.underline_position = -7
+  M.dpi = 164
 else
-    M.font = wezterm.font("Cascadia Code")
-    M.font_size = 13.5
-    M.bold_brightens_ansi_colors = true
-    M.freetype_load_target = "Light"
-    M.line_height = 1.05
-    M.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
-    M.underline_position = -7
+  M.font = wezterm.font("Cascadia Code")
+  M.font_size = 13.5
+  M.bold_brightens_ansi_colors = true
+  M.freetype_load_target = "Light"
+  M.line_height = 1.05
+  M.harfbuzz_features = { "calt=1", "clig=1", "liga=1" }
+  M.underline_position = -7
 end
 
 M.enable_kitty_graphics = true
