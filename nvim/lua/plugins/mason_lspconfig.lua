5c82f6c6c03da633cc61b3fe9958e6c01e897726
Peiran Du <dupeiran3@gmail.com>
2025-05-20 16:16:44 -0500

    fix: migrate config to use new vim.lsp.config feature

--- nvim/lua/plugins/mason_lspconfig.lua
+++ nvim/lua/plugins/mason_lspconfig.lua
@@ -1,82 +1,99 @@
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = {
-		"williamboman/mason.nvim",
-		"williamboman/mason-lspconfig.nvim",
-		"saghen/blink.cmp",
-	},
-	event = { "BufReadPre", "BufNewFile" },
-	lazy = true,
-	opts = {
+  "williamboman/mason-lspconfig.nvim",
+  dependencies = {
+    "williamboman/mason.nvim",
+    -- "williamboman/mason-lspconfig.nvim", -- This is the main plugin, no need to list as its own dependency
+    "saghen/blink.cmp",
+  },
+  event = { "BufReadPre", "BufNewFile" },
+  lazy = true,
+  opts = {
+    -- A list of servers to automatically install if they're not already installed.
+    ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "bashls", "cmake", "marksman", "pyright", "taplo" },
 
-		-- A list of servers to automatically install if they're not already installed. Example: { "rust_analyzer@nightly", "lua_ls" }
-		-- This setting has no relation with the `automatic_installation` setting.
-		---@type string[]
-		ensure_installed = { "lua_ls", "rust_analyzer", "clangd", "bashls", "cmake", "marksman", "pyright", "taplo" },
+    -- Whether servers that are set up (via lspconfig) should be automatically installed.
+    automatic_installation = false,     -- Can be true or { exclude = { ... } }
 
-		-- Whether servers that are set up (via lspconfig) should be automatically installed if they're not already installed.
-		-- This setting has no relation with the `ensure_installed` setting.
-		-- Can either be:
-		--   - false: Servers are not automatically installed.
-		--   - true: All servers set up via lspconfig are automatically installed.
-		--   - { exclude: string[] }: All servers set up via lspconfig, except the ones provided in the list, are automatically installed.
-		--       Example: automatic_installation = { exclude = { "rust_analyzer", "solargraph" } }
-		---@type boolean
-		automatic_installation = false,
+    -- Handlers definition now directly part of opts
+    handlers = {
+      -- This first function is the default handler and will be called for each installed server
+      -- that doesn't have a dedicated handler.
+      function(server_name)       -- Default handler
+        local lspconfig = require("lspconfig")
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+        lspconfig[server_name].setup({
+          capabilities = capabilities,
+        })
+      end,
 
-		-- See `:h mason-lspconfig.setup_handlers()`
-		---@type table<string, fun(server_name: string)>?
-		handlers = nil,
-	},
-	config = function(_, opts)
-		local capabilities = require("blink.cmp").get_lsp_capabilities()
-		require("mason-lspconfig").setup(opts)
-		require("mason-lspconfig").setup_handlers({
-			-- The first entry (without a key) will be the default handler
-			-- and will be called for each installed server that doesn't have
-			-- a dedicated handler.
-			function(server_name) -- default handler (optional)
-				require("lspconfig")[server_name].setup({ capabilities = capabilities })
-			end,
-			-- Next, you can provide a dedicated handler for specific servers.
-			-- For example, a handler override for the `rust_analyzer`:
-			-- ["rust_analyzer"] = function ()
-			--     require("rust-tools").setup {}
-			-- end
+      -- Dedicated handler for lua_ls
+      ["lua_ls"] = function()
+        local lspconfig = require("lspconfig")
+        local capabilities = require("blink.cmp").get_lsp_capabilities()
+        lspconfig.lua_ls.setup({
+          capabilities = capabilities,
+          on_init = function(client)
+            -- Check if workspace_folders is not nil and has at least one entry
+            if client.workspace_folders and client.workspace_folders[1] and client.workspace_folders[1].name then
+              local path = client.workspace_folders[1].name
+              if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
+                return
+              end
+            end
+            -- Ensure client.config.settings.Lua exists before trying to extend it
+            client.config.settings = client.config.settings or {}
+            client.config.settings.Lua = client.config.settings.Lua or {}
 
-			["lua_ls"] = function()
-				require("lspconfig").lua_ls.setup({
-					capabilities = capabilities,
-					on_init = function(client)
-						local path = client.workspace_folders[1].name
-						if vim.loop.fs_stat(path .. "/.luarc.json") or vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-							return
-						end
-						client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
-							runtime = {
-								-- Tell the language server which version of Lua you're using
-								-- (most likely LuaJIT in the case of Neovim)
-								version = "LuaJIT",
-							},
-							-- Make the server aware of Neovim runtime files
-							workspace = {
-								checkThirdParty = false,
-								library = {
-									vim.env.VIMRUNTIME,
-									-- Depending on the usage, you might want to add additional paths here.
-									-- "${3rd}/luv/library"
-									-- "${3rd}/busted/library",
-								},
-								-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-								-- library = vim.api.nvim_get_runtime_file("", true)
-							},
-						})
-					end,
-					settings = {
-						Lua = {},
-					},
-				})
-			end,
-		})
-	end,
+            client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+              runtime = {
+                version = "LuaJIT",
+              },
+              workspace = {
+                checkThirdParty = false,
+                library = {
+                  vim.env.VIMRUNTIME,
+                },
+              },
+            })
+          end,
+          settings = {
+            Lua = {},             -- Initial settings, on_init will modify/extend this
+          },
+        })
+      end,
+
+      -- Example: Dedicated handler for rust_analyzer (if you were using rust-tools)
+      -- ["rust_analyzer"] = function ()
+      --   require("rust-tools").setup {} -- You would need to have rust-tools.nvim installed
+      -- end
+
+      -- Add other specific server handlers here, for example:
+      -- ["pyright"] = function()
+      --   local lspconfig = require("lspconfig")
+      --   local capabilities = require("blink.cmp").get_lsp_capabilities()
+      --   lspconfig.pyright.setup({
+      --     capabilities = capabilities,
+      --     settings = {
+      --       python = {
+      --         analysis = {
+      --           typeCheckingMode = "basic", -- or "strict"
+      --         }
+      --       }
+      --     }
+      --   })
+      -- end,
+    },
+  },
+  config = function(_, opts)
+    -- The opts table here is the one defined above, including your handlers.
+    -- mason-lspconfig will now use opts.handlers internally.
+    require("mason-lspconfig").setup(opts)
+
+    -- You can still add other general LSP configurations here if needed,
+    -- for example, global mappings for LSP actions, but server-specific setup
+    -- should be within the handlers.
+    -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
+    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
+    -- etc.
+  end,
 }
