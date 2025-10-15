578266f961002b3a882bfe4d1f7dd3707a57e1fc
Peiran Du <dupeiran3@gmail.com>
2025-10-15 10:45:47 -0500

    fix: conflict

--- nvim/lua/plugins/neoclip.lua
+++ nvim/lua/plugins/neoclip.lua
@@ -1,18 +1,17 @@
 return {
-  "AckslD/nvim-neoclip.lua",
-  dependencies = {
-    { 'kkharji/sqlite.lua', module = 'sqlite' },
-    -- you'll need at least one of these
-    -- {'nvim-telescope/telescope.nvim'},
-    -- {'ibhagwan/fzf-lua'},
-  },
-  config = function()
-    require('neoclip').setup({
-      history = 1000,
-      length_limit = 1048576,
-      continuous_sync = true,
-      enable_persistent_history = true,
-      db_path = vim.fn.stdpath("data") .. "/databases/neoclip.sqlite3",
-    })
-  end,
+	"AckslD/nvim-neoclip.lua",
+	dependencies = {
+		{ "kkharji/sqlite.lua", module = "sqlite" },
+		-- you'll need at least one of these
+		-- {'nvim-telescope/telescope.nvim'},
+		-- {'ibhagwan/fzf-lua'},
+	},
+	config = function()
+		require("neoclip").setup({
+			history = 1000,
+			length_limit = 1048576,
+			continuous_sync = true,
+			enable_persistent_history = false,
+		})
+	end,
 }
