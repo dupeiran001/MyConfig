46c9e73829b8c0571fee70995ac287264cde4225
Peiran Du <dupeiran3@gmail.com>
2026-05-01 18:51:10 -0500

    feat: use relm rebuild of rs-bar

--- niri/launch-rs-bar.sh
+++ niri/launch-rs-bar.sh
@@ -4,7 +4,7 @@
 # Asahi Linux on Apple Silicon -> macbook, otherwise -> intel.
 
 if [ -d /sys/class/power_supply/macsmc-battery ] || grep -qi 'apple' /proc/cpuinfo 2>/dev/null; then
-    exec rs-bar --config macbook
+    exec rs-bar --config macbook --relm
 else
-    exec rs-bar --config intel
+    exec rs-bar --config intel --relm
 fi
